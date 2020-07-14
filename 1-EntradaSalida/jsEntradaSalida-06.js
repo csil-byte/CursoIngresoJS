b5876176841734ab15c62b02e4896d8be8d5fab4
csil-byte <cgscsilva@gmail.com>
2020-07-13 22:21:32 -0300

    Clase 1, 130720 ej 1-6 ES

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,19 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
-}
+	/*var numeroUno;
+	var numeroDos;
+	var suma;
+	numeroUno= parseInt(txtIdNumeroUno.value);
+	numeroDos= parseInt(txtIdNumeroDos.value);
+	
+	suma = (numeroUno + numeroDos);
 
+	alert('La suma es ' + suma);*/
+	var numeroUno= parseInt(txtIdNumeroUno.value);
+	var numeroDos= parseInt(txtIdNumeroDos.value);
+	var suma= (numeroUno + numeroDos);
+	alert('La suma es ' + suma);
+	
+
+}
